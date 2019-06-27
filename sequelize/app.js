e3cabf93fc8c65d5b4a6f8e874f986370a600c1a
Diego Sarmento Mendes <diego.s.mendes2310@gmail.com>
2019-06-27 06:33:53 -0300

    Conhecendo o sequelize

---
+++ sequelize/app.js
@@ -0,0 +1,90 @@
+const Sequelize = require('sequelize');
+
+// Configurando conexão
+const sequelize = new Sequelize('postgres://postgres:postgres@localhost:5432/sequelize');
+
+// Testando conexão no banco de dados
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Criando uma entidade (model)
+const User = sequelize.define('user', {
+  // attributes
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING
+    // allowNull defaults to true
+  }
+}, {
+    // options
+  });
+
+// Find all users
+User.findAll().then(users => {
+  console.log("All users:", JSON.stringify(users, null, 4));
+});
+
+// Create a new user
+User.create({ firstName: "Jane", lastName: "Doe" }).then(jane => {
+  console.log("Jane's auto-generated ID:", jane.id);
+});
+
+// Delete everyone named "Jane"
+User.destroy({
+  where: {
+    firstName: "Jane"
+  }
+}).then(() => {
+  console.log("Done");
+});
+
+// Change everyone without a last name to "Doe"
+User.update({ lastName: "Doe" }, {
+  where: {
+    lastName: null
+  }
+}).then(() => {
+  console.log("Done");
+});
+
+class Employee extends Sequelize.Model {}
+Employee.init({
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    get() {
+      const title = this.getDataValue('title');
+      // 'this' allows you to access attributes of the instance
+      return this.getDataValue('name') + ' (' + title + ')';
+    },
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    set(val) {
+      this.setDataValue('title', val.toUpperCase());
+    }
+  }
+}, { sequelize, modelName: 'employee' });
+
+sequelize.sync();
+
+Employee
+  .create({ name: 'John Doe', title: 'senior engineer' })
+  .then(employee => {
+    console.log(employee.get('name')); // John Doe (SENIOR ENGINEER)
+    console.log(employee.get('title')); // SENIOR ENGINEER
+  })
+
+
+// Cria ou dropa e cria tabelas no banco
+sequelize.sync();
